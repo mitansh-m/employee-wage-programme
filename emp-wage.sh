01d7fb90a0f8d05971ff4b7e1ee4c3a5c9d54d35
mitansh-m <mitansh.hsnatim@gmail.com>
2023-05-03 17:01:19 +0530

    succesful attendance categorization

--- emp-wage.sh
+++ emp-wage.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
 readonly daily_wage_per_hour=20
-readonly daily_hour=8 
 echo Welcome to Employee Wage Computation Programme
 echo -----------------------------------------------
-attendance=$(($RANDOM % 2))
+attendance=$(($RANDOM % 3))
 if (($attendance==0)); then 
-    echo 'employee is present'
-else 
     echo 'employee is absent'
+    working_hour=0
+    attendance_alphabetic="absent"
+elif (($attendance==1)); then 
+    echo 'employee is full time'
+    working_hour=8
+    attendance_alphabetic="full time"
+else
+    echo 'employee is part time'
+    working_hour=4
+    attendance_alphabetic="part time"
 fi 
 echo -----------------------------------------------
-echo $(($daily_wage_per_hour*$daily_hour)) is the daily wage for full time employee
+echo $(($daily_wage_per_hour*$working_hour)) is the daily wage for $attendance_alphabetic employee
