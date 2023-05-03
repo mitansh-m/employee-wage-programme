d49acb8de40a02f07f9bc332b2eb1c7d190731bc
mitansh-m <mitansh.hsnatim@gmail.com>
2023-05-03 09:33:39 +0530

    attendance checker

--- emp-wage.sh
+++ emp-wage.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 echo Welcome to Employee Wage Computation Programme
+echo -----------------------------------------------
+attendance=$(($RANDOM % 2))
+if (($attendance==0)); then 
+    echo 'employee is present'
+else 
+    echo 'employee is absent'
+fi 
