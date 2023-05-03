a95d93f3c99b320974b653fdb6cef6a4b2a0e142
mitansh-m <mitansh.hsnatim@gmail.com>
2023-05-03 09:46:57 +0530

    calculation of daily wage

--- emp-wage.sh
+++ emp-wage.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+readonly daily_wage_per_hour=20
+readonly daily_hour=8 
 echo Welcome to Employee Wage Computation Programme
 echo -----------------------------------------------
 attendance=$(($RANDOM % 2))
@@ -8,3 +10,5 @@ if (($attendance==0)); then
 else 
     echo 'employee is absent'
 fi 
+echo -----------------------------------------------
+echo $(($daily_wage_per_hour*$daily_hour)) is the daily wage for full time employee
