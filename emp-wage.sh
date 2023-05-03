fa0c9320e3ec37c5297acef9a400c5ba2f336e4a
mitansh-m <mitansh.hsnatim@gmail.com>
2023-05-03 17:17:31 +0530

    updated switchcase

--- emp-wage.sh
+++ emp-wage.sh
@@ -4,18 +4,18 @@ readonly daily_wage_per_hour=20
 echo Welcome to Employee Wage Computation Programme
 echo -----------------------------------------------
 attendance=$(($RANDOM % 3))
-if (($attendance==0)); then 
-    echo 'employee is absent'
-    working_hour=0
-    attendance_alphabetic="absent"
-elif (($attendance==1)); then 
-    echo 'employee is full time'
-    working_hour=8
-    attendance_alphabetic="full time"
-else
-    echo 'employee is part time'
-    working_hour=4
-    attendance_alphabetic="part time"
-fi 
-echo -----------------------------------------------
+case $attendance in  
+        0)
+        attendance_alphabetic='full time'
+        working_hour=8
+        ;;
+        1) 
+        attendance_alphabetic='part time'
+        working_hour=4
+        ;;
+        *)
+        attendance_alphabetic='absent'
+        working_hour=0
+        ;;
+esac
 echo $(($daily_wage_per_hour*$working_hour)) is the daily wage for $attendance_alphabetic employee
