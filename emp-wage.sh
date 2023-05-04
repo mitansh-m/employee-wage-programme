bd7050427e0c536a84335c6e97fbf88222599983
mitansh-m <mitansh.hsnatim@gmail.com>
2023-05-04 06:11:23 +0530

    indented and finalized

--- emp-wage.sh
+++ emp-wage.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 readonly wage_per_hour=20
+
 echo Welcome to Employee Wage Computation Programme
 echo -----------------------------------------------
+
 function dailywagecalc(){
 attendance=$(($RANDOM % 3))
 case $attendance in  
@@ -17,13 +19,16 @@ case $attendance in
         ;;
 esac
 daily_wage=$(($working_hour*$wage_per_hour))
- }
+}
+
 day=0 
 hour=0
+
 while (($hour<=100 && $day<=20)); do 
         dailywagecalc
         day=$(($day+1))
         hour=$(($hour+$working_hour))
         totalwage=$(($totalwage+$daily_wage))
 done 
-echo "$totalwage is made by employee in one month (20 days)"
+
+echo "$totalwage is made by employee in one month"
