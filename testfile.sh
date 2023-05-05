8629b17ecd865200474448664c7f42897faeda2f
mitansh-m <mitansh.hsnatim@gmail.com>
2023-05-05 14:20:13 +0530

    code efficiency managed [ version 1 ]

--- testfile.sh
+++ testfile.sh
@@ -1,31 +1,42 @@
 #!/bin/bash
 
 readonly wage_per_hour=20
+day=0
 echo Welcome to Employee Wage Computation Programme
-echo -----------------------------------------------
-function dailywagecalc(){
-attendance=$(($RANDOM % 3))
-case $attendance in  
+
+function dailywagecalc() {
+        attendance=$(($RANDOM % 3))
+        case $attendance in
         0)
-        working_hour=8
-        ;;
-        1) 
-        working_hour=4
-        ;;
+                working_hour=8
+                ;;
+        1)
+                working_hour=4
+                ;;
         *)
-        working_hour=0
-        ;;
-esac
-daily_wage=$(($working_hour*$wage_per_hour))
- }
-day=0 
-while (($day < 20)); do 
-        dailywagecalc
-        day=$(($day+1))
-        totalwage=$(($totalwage+$daily_wage))
-        declare -a TheWorkingDay
-        TheWorkingDay[$day]=$daily_wage
-        echo  $day   :   ${TheWorkingDay[$day]}
-done 
+                working_hour=0
+                ;;
+        esac
+        daily_wage=$(($working_hour * $wage_per_hour))
+}
+
+function declaration() {
+        echo $day : ${TheWorkingDay[$day]}
+}
+
+declare -a TheWorkingDay
+
+function array_assignment() {
+        while (($day < 20)); do
+                dailywagecalc
+                day=$(($day + 1))
+                totalwage=$(($totalwage + $daily_wage))
+                TheWorkingDay[$day]=$daily_wage
+                declaration
+        done
+}
+
+array_assignment
+
 echo -----------------------------------------------------------------
 echo "$totalwage is made by employee in one month (${#TheWorkingDay[@]} days)"
