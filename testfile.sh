a639593ca0e4231f3766096de2d8d32ba4661048
mitansh-m <mitansh.hsnatim@gmail.com>
2023-05-05 10:20:17 +0530

    array implementation in lieu of regular variables

---
+++ testfile.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+readonly wage_per_hour=20
+echo Welcome to Employee Wage Computation Programme
+echo -----------------------------------------------
+function dailywagecalc(){
+attendance=$(($RANDOM % 3))
+case $attendance in  
+        0)
+        working_hour=8
+        ;;
+        1) 
+        working_hour=4
+        ;;
+        *)
+        working_hour=0
+        ;;
+esac
+daily_wage=$(($working_hour*$wage_per_hour))
+ }
+day=0 
+while (($day < 20)); do 
+        dailywagecalc
+        day=$(($day+1))
+        totalwage=$(($totalwage+$daily_wage))
+        declare -a TheWorkingDay
+        TheWorkingDay[$day]=$daily_wage
+        echo  employee made ${TheWorkingDay[$day]} on day $day
+done 
+echo 
+echo "$totalwage is made by employee in one month (${#TheWorkingDay[@]} days)"
